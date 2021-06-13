084fb5c45716eb2102b5c94b4e2f8ac1920f80f4
JonatanLima <jotanlima@gmail.com>
2021-06-13 18:48:06 -0300

    feat: add study of assert module

---
+++ nodejs/modules/assert/index.js
@@ -0,0 +1,54 @@
+const assert = require('assert')
+const AssertionError = require('assert').AssertionError
+
+// Test #1 - strictEqual
+try {
+  console.log("++++++++++++++++++++++++++++++++++++")
+  // assert.strictEqual(5, 10, "The values are not equal") // fail
+  assert.strictEqual(5, 5, "The values are not equal")
+  console.log("[TEST_SUCCESS]: Should receive same values")
+} catch (error) {
+  if (error instanceof AssertionError) {
+    console.error(error)
+  }
+}
+
+// Test #2 - deepStrictEqual
+try {
+  console.log("++++++++++++++++++++++++++++++++++++")
+  // fail
+  // assert.deepStrictEqual(
+  //   { name: "Jonatan Lima", age: 26 },
+  //   { name: "Carolina Patrício", age: 27},
+  //    "The objects are equals"
+  // )
+  assert.deepStrictEqual(
+    { name: "Jonatan Lima", age: 26 },
+    { name: "Jonatan Lima", age: 26 },
+    "The objects are equals"
+  )
+  console.log("[TEST_SUCCESS]: Should the objects are equals")
+} catch (error) {
+  if (error instanceof AssertionError) {
+    console.error(error)
+  }
+}
+
+// Test #3 - reject promise
+try {
+  console.log("++++++++++++++++++++++++++++++++++++")
+  assert.rejects(async () => {
+    return await Promise.reject({
+      name: "John",
+      age: "20"
+    });
+  }, {
+    name: "John",
+    age: "20"
+  }, "Promise resolved!");
+  console.log("[TEST_SUCCESS]: Should Promise resolved")
+} catch (error) {
+  if (error instanceof AssertionError) {
+    console.error(error)
+  }
+}
