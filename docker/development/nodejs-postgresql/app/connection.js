00fa7bd42b4d5813e0c83c108703896f55daeaf3
JonatanLima <jotanlima@gmail.com>
2021-06-14 14:57:45 -0300

    feat: add configs to development nodejs by docker

---
+++ docker/development/nodejs-postgresql/app/connection.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const config = require('./config.json')
+
+const { Client } = require('pg')
+
+const client = new Client({
+	user: config.database.POSTGRES_USER,
+	host: config.database.POSTGRES_HOST,
+	database: config.database.POSTGRES_DB,
+	password: config.database.POSTGRES_PASSWORD
+})
+
+module.exports = client
