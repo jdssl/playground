0a7a1fbb38bd0b931a224938ac8e680863e82e0e
JonatanLima <jotanlima@gmail.com>
2021-06-14 21:45:11 -0300

    feat: init knex study

---
+++ knexjs/app/index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const config = require('./config')
+
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host : config.database.POSTGRES_HOST,
+    user : config.database.POSTGRES_USER,
+    password : config.database.POSTGRES_PASSWORD,
+    database : config.database.POSTGRES_DB
+  }
+})
