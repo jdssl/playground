46e3b984dedd7ad0c5ee88aee1c9989c4e717421
JonatanLima <jotanlima@gmail.com>
2021-06-14 21:53:26 -0300

    feat: knex toSQL

--- knexjs/app/index.js
+++ knexjs/app/index.js
@@ -9,5 +9,14 @@ const knex = require('knex')({
     user : config.database.POSTGRES_USER,
     password : config.database.POSTGRES_PASSWORD,
     database : config.database.POSTGRES_DB
+  },
+  migrations: {
+    tableName: 'migrations'
   }
 })
+
+const result = knex('persons')
+  .select()
+  .toSQL()
+
+console.log(result)
