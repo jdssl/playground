678754e9c06eb0c44a35bdfbc2861ea093974c53
Jonatan Lima <jotanlima@gmail.com>
2021-07-05 16:54:38 -0300

    feat: stream process

--- nodejs/modules/streams/05-example.mjs
+++ nodejs/modules/streams/05-example.mjs
@@ -1,5 +1,6 @@
 import net from 'net'
 
 net.createServer(socket => socket.pipe(process.stdout)).listen(1338)
+// Pipe is DANGER!!!! MEMORY LEAK use pipeline of stream
 
 // node -e "process.stdin.pipe(require('net').connect(1338))"
