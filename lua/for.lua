662602c41959ae567869fea94848d62d718b7eac
Jonatan Lima <jotanlima@gmail.com>
2021-11-23 07:51:56 -0300

    feat: for and repeat

---
+++ lua/for.lua
@@ -0,0 +1,13 @@
+-- For
+
+num = 0
+for i = 1, 100 do -- the range includes both ends
+	num = num + 1
+end
+print(num)
+
+sum = 0
+for j = 100, 1, -1 do sum = sum + j end
+print(sum)
+
+
---
+++ lua/repeat.lua
@@ -0,0 +1,7 @@
+-- Repeat, another loop construct
+
+num = 10
+repeat
+	print('Lua for neovim')
+	num = num - 1
+until num == 0
