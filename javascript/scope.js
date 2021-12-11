0d2f4d0c4d476d62c1028ab23bfbfcedbe137887
Jonatan Lima <jotanlima@gmail.com>
2021-12-11 17:32:12 -0300

    feat: practice

---
+++ javascript/scope.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const foo = function () {
+	let a = 3, b = 5;
+	const bar = function() {
+		let b = 7, c = 11;
+
+		a += b + c; // b is 7 and c is 11
+		// 3 + 7 + 11
+	}
+	bar()
+	// a is 21 and b is 5
+}
+
+foo()
