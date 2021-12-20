3a2942ae5cbfeaaa39a2e44a23ebe7bc0fca352c
Jonatan Lima (balu) <jotanlima@gmail.com>
2021-12-20 08:32:05 -0300

    feat: add client

--- nodejs/modules/socket/index.js
+++ nodejs/modules/socket/index.js
@@ -11,8 +11,13 @@ const server = http.createServer(app)
 const wsServer = new webSocketServer({ server: server })
 
 wsServer.on("connection", function(ws) {
+  ws.on("close", function(message, flags) {
+    console.log('disconnected')
+  })
+
   ws.on("message", function(message, flags) {
     ws.send(message, flags)
+    console.log('message', message.toString())
   })
 })
 
