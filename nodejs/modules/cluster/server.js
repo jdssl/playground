f54f56c771df0c193bb754184599edfb7ba1f8d1
Jonatan Lima (balu) <jotanlima@gmail.com>
2021-12-25 06:50:17 -0300

    feat: cluster

---
+++ nodejs/modules/cluster/server.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const http = require('http')
+const cluster = require('cluster')
+const numCPUs = require('os').cpus().length
+
+const PORT = 3000
+
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`)
+
+  for (let i = 0; i < numCPUs; i++) {
+    console.log('forking')
+    cluster.fork()
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${process.pid} died`)
+  })
+
+} else {
+  http
+    .createServer((req, res) => {
+      console.log(`${process.pid}: request ${req.url}`)
+      res.writeHead(200)
+      res.end('Hi Balu')
+    })
+    .listen(PORT, console.log(`server running at http://localhost:${PORT}`))
+}
+
+
