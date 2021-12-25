bca84290b022390be99811e50e1d971b27cfa04d
Jonatan Lima (balu) <jotanlima@gmail.com>
2021-12-25 06:55:32 -0300

    feat: add cluster events

--- nodejs/modules/cluster/server.js
+++ nodejs/modules/cluster/server.js
@@ -9,6 +9,14 @@ const PORT = 3000
 if (cluster.isMaster) {
   console.log(`Master ${process.pid} is running`)
 
+  cluster.on('fork', (worker) => {
+    console.log('Trying forking the workers')
+  })
+
+  cluster.on('online', (worker) => {
+    console.log('Workers forking succeed')
+  })
+
   for (let i = 0; i < numCPUs; i++) {
     console.log('forking')
     cluster.fork()
