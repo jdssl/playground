4d368930ff8eb235b6fc473106635a4cf9c1018c
Jonatan Lima (balu) <jotanlima@gmail.com>
2021-12-25 07:23:22 -0300

    feat: kill all worker by iteration

---
+++ nodejs/modules/cluster/workers.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const http = require('http')
+const cluster = require('cluster')
+const numCPUs = require('os').cpus().length
+
+if (cluster.isMaster) {
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork()
+  }
+
+  for (let id in cluster.workers) {
+    console.log(`Kill ${id}`)
+    cluster.workers[id].kill()
+  }
+}
