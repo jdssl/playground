d7d14c2624365d3db69e87ba439fae68d48e349d
Jonatan Lima (balu) <jotanlima@gmail.com>
2022-01-18 21:16:43 -0300

    feat: render dummy list of articles

--- rust/headlines/headlines/src/main.rs
+++ rust/headlines/headlines/src/main.rs
@@ -1,11 +1,36 @@
 use eframe::{egui::CentralPanel, epi::App, run_native, NativeOptions};
 
-struct Headlines;
+struct Headlines {
+    articles: Vec<NewsCardData>,
+}
+
+impl Headlines {
+    fn new() -> Headlines {
+        let iter = (0..20).map(|a| NewsCardData {
+            title: format!("title{}", a),
+            description: format!("desc{}", a),
+            url: format!("https://example.com/{}", a),
+        });
+        Headlines {
+            articles: Vec::from_iter(iter),
+        }
+    }
+}
+
+struct NewsCardData {
+    title: String,
+    description: String,
+    url: String,
+}
 
 impl App for Headlines {
     fn update(&mut self, ctx: &eframe::egui::CtxRef, _frame: &mut eframe::epi::Frame<'_>) {
         CentralPanel::default().show(ctx, |ui| {
-            ui.label("article text");
+            for a in &self.articles {
+                ui.label(&a.title);
+                ui.label(&a.url);
+                ui.label(&a.description);
+            }
         });
     }
 
@@ -15,7 +40,7 @@ impl App for Headlines {
 }
 
 fn main() {
-    let app = Headlines;
+    let app = Headlines::new();
     let win_option = NativeOptions::default();
     run_native(Box::new(app), win_option);
 }
