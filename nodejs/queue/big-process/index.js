4014a781fc832f4b8f2b010e27413f9db81980e4
Jonatan Lima (balu) <jotanlima@gmail.com>
2022-02-26 16:53:49 -0300

    simple queue

--- nodejs/queue/big-process/index.js
+++ nodejs/queue/big-process/index.js
@@ -1,12 +1,46 @@
 import http from 'http'
 import { spawn } from 'child_process'
 import path from 'path'
+import os from 'os'
 
 const PORT = 2602
 
+const systemCpuCore = os.cpus()
+const maxParallelProcess = systemCpuCore.length / 2
+let runningProcs = []
+
+async function runBigProcessInQueue() {
+  if (runningProcs.length >= maxParallelProcess) {
+    console.log('queue is full, waiting some process finish')
+    await runningProcs[0]
+    return runBigProcessInQueue()
+  }
+
+  console.log('running process...')
+
+  const promise = bigProcess()
+  runningProcs.push(promise)
+
+  function removePromise() {
+    console.log('proc done, removing...')
+    runningProcs = runningProcs.filter(p => p !== promise)
+  }
+
+  let result;
+
+  try {
+    result = await promise
+  } catch (err) {
+    throw err
+  } finally {
+    removePromise()
+  }
+
+  return result
+}
+
 async function bigProcess() {
   return new Promise((resolve, reject) => {
-
     const { pathname: currentFile } = new URL(import.meta.url);
     const cwd = path.dirname(currentFile)
 
@@ -16,7 +50,7 @@ async function bigProcess() {
 
     const stderr = []
 
-    proc.stdout.on('data', (chunk) => console.log(chunk.toString()))
+    proc.stdout.on('data', (chunk) => { /* console.log(chunk.toString()) */ })
 
     proc.stderr.on('data', (chunk) => stderr.push(chunk))
 
@@ -37,7 +71,8 @@ http
     if (req.url === '/big') {
       const started = new Date()
 
-      await bigProcess()
+      // await bigProcess()
+      await runBigProcessInQueue()
 
       console.log(`This process took: ${new Date() - started}ms`)
       return res.end('big')
--- nodejs/queue/big-process/sub-process.js
+++ nodejs/queue/big-process/sub-process.js
@@ -1,5 +1,5 @@
 function bigProcess() {
-  for (let i = 0; i < 50000; ++i) {
+  for (let i = 0; i < 500000; ++i) {
     console.log(i)
     // process.stdout.write(String(i))
   }
