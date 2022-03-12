5c093c5c00b13f3dfdc3458ef5789ad37ac81879
Jonatan Lima (balu) <jotanlima@gmail.com>
2022-03-12 14:44:17 -0300

    refactor code

--- nodejs/recursive/scripts/run.js
+++ nodejs/recursive/scripts/run.js
@@ -3,26 +3,41 @@
 import axios from 'axios'
 
 import logger from './../common/helpers/logger.js'
+import { sleep } from './../common/helpers/sleep.js'
 
 const URL = 'http://localhost:1203'
 
 async function getData(id, name) {
-  logger.info(`${id}, ${name}`)
+  logger.info(`params: ${id}, ${name}`)
   return axios.get(`${URL}/data`)
 }
 
 async function postData(body) {
-  console.log(body)
-  return axios.post(`${URL}/data`, JSON.stringify(body))
+  logger.info(`body: ${JSON.stringify(body)}`)
+  return axios.post(`${URL}/data`, body)
 }
 
 async function recursiveFn(params, callback, tries = 3) {
+  if (tries === 0) {
+    throw new Error('All request tries failed');
+  }
+
   const res = await callback.apply(this, params)
-  logger.info(res.data)
+  logger.info(`response: ${JSON.stringify(res.data)}`)
+
+  sleep(500)
+
+  await recursiveFn(params, callback, tries - 1)
+
+  logger.info(tries - 1)
 }
 
 ;(async () => {
   const body =[{ name: 'Balu', age: 27 }]
-  await recursiveFn([1, 'balu'], getData)
-  await recursiveFn(body, postData)
+
+  return Promise.all([
+   recursiveFn([1, 'balu'], getData),
+   recursiveFn(body, postData)
+  ])
 })()
+
