09e0bcc4f2596aede32898c100768c5f4e23cdc0
Jonatan Lima (balu) <jotanlima@gmail.com>
2022-09-04 13:46:20 -0300

    feat: improve closures

--- rust/closures/src/main.rs
+++ rust/closures/src/main.rs
@@ -1,20 +1,19 @@
 fn main() {
     let str = '😀'.to_string();
     // The clousure use implict move
-    let f = || {
+    let f = move || {
         println!("{}", str);
     };
-    // println!("{}", str);
+    // println!("{}", str); // Error beacuse ownership, with `move` keyword
     f();
 
     let v = vec![1, 2, 3, 4];
 
     let r: i32 = v.iter().map(|x| x * 3).filter(|x| *x > 10).sum();
 
-    println!("vec: {:?}", v);
-    println!("r: {:?}", r);
-
     let add = |x, y| x + y;
 
+    println!("vec: {:?}", v);
+    println!("r: {:?}", r);
     println!("add: {}", add(1, 2));
 }
