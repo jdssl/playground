0f7f02e7dfb59fe62175566ca597986111f8dd6c
balu <jotanlima@gmail.com>
2023-10-07 16:55:05 -0300

    add simple service

---
+++ go/mock/domain/service.go
@@ -0,0 +1,53 @@
+package domain
+
+import "math/rand"
+
+func randomString(n int) string {
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+	s := make([]rune, n)
+	for i := range s {
+		s[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(s)
+}
+
+type Note struct {
+	ID          string
+	Title       string
+	Description string
+}
+
+func NewNote(title, description string) (*Note, error) {
+	return &Note{
+		ID:          randomString(10),
+		Title:       title,
+		Description: description,
+	}, nil
+}
+
+type Repository interface {
+	Save(note *Note) error
+}
+
+type Service interface {
+	Create(newNote Note) (string, error)
+}
+
+type ServiceImpl struct {
+	Repository Repository
+}
+
+func (s *ServiceImpl) Create(newNote Note) (string, error) {
+	note, err := NewNote(newNote.Title, newNote.Description)
+	if err != nil {
+		return "", err
+	}
+
+	err = s.Repository.Save(note)
+	if err != nil {
+		return "", err
+	}
+
+	return note.ID, nil
+}
